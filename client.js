d9a8466481be4ed1f0fe1cffc41367e3a9823e4d
jackieparke <jparke1012@gmail.com>
2026-06-04 16:54:42 -0400

    still fixing password reset

--- client.js
+++ client.js
@@ -5,4 +5,11 @@ import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js
 const SUPABASE_URL = 'https://lacbkdudcjebllcshmhi.supabase.co';
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxhY2JrZHVkY2plYmxsY3NobWhpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzkxMDY4OTUsImV4cCI6MjA5NDY4Mjg5NX0.MHX_DTIDFYm7eAQJjkwy_QLrzELyHo2pB0FtBgFBwYA';
 
-export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
+  global: {
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    }
+  }
+});
